d8591473b4d85ead4bc5ff9eaf5de7fd65aae4f7
BugHunter-X <askzenai@gmail.com>
2025-02-11 16:14:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return null; //This line causes error
+  }
+  return a + b;
+}
+console.log(foo(null, 5)); // Output: null
+console.log(foo(5, null)); // Output: null
+console.log(foo(5, 5)); // Output: 10
